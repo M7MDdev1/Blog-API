dd0c33f92156351c9a91a8887ffca30dc8de9983
Mohammed27800 <asmte2780@gmail.com>
2024-11-11 11:25:59 +0300

    Add Application json header

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,25 +1,38 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Header,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postServices: PostsService) {}
+
   @Post()
+  @Header('content-type', 'application/json')
   async create(@Body() boody) {
     return this.postServices.create(boody);
   }
 
   @Get()
+  @Header('content-type', 'application/json')
   async getPosts() {
     return this.postServices.getPosts();
   }
 
   @Get(':id')
+  @Header('content-type', 'application/json')
   async getPost(@Param('id') postID) {
     return this.postServices.getPost(postID);
   }
 
   @Delete(':id')
+  @Header('content-type', 'application/json')
   async deletePost(@Param('id') PostID) {
     return this.postServices.deletePost(PostID);
   }
